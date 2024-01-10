d8f3500255787eaec95952559830b4e7413cc338
Eugene Dubinin <eugend@commandprompt.com>
2024-01-10 13:53:17 +0200

    redmine 5 version removed "remove empty fields" option since it is now handled by redmine natively

--- init.rb
+++ init.rb
@@ -4,25 +4,15 @@ Redmine::Plugin.register :redmine_tidy_emails do
   name 'Redmine Tidy Emails'
   author 'Eugene Dubinin <eugend@commandprompt.com>'
   description 'This plugin allows to customize what to include in issue email notifications'
-  version '0.1.1'
+  version '0.1.2'
   author_url 'https://www.commandprompt.com'
-  requires_redmine :version_or_higher => '3.0.x'
+  requires_redmine :version_or_higher => '5.0'
 
   settings default: {
-      'remove_empty_fields': false,
       'remove_issue_edit_description': false,
       'remove_header_on_update': false
     }, partial: 'settings/redmine_tidy_emails'
 end
 
-prepare_block = Proc.new do
-  IssuesHelper.send(:include, RedmineTidyEmails::IssuesHelperPatch)
-  Mailer.send(:include, RedmineTidyEmails::MailerPatch)
-  Mailer.layout 'mailer_alt'
-end
-
-if Rails.env.development?
-  ((Rails.version > "5")? ActiveSupport::Reloader : ActionDispatch::Callbacks).to_prepare { prepare_block.call }
-else
-  prepare_block.call
-end
+Mailer.send(:include, RedmineTidyEmails::MailerPatch)
+Mailer.layout 'mailer_alt'
--- lib/redmine_tidy_emails.rb
+++ lib/redmine_tidy_emails.rb
@@ -1,30 +1,4 @@
 module RedmineTidyEmails
-    module IssuesHelperPatch
-    unloadable
-
-    def self.included(base)
-      base.class_eval do
-        alias_method :email_issue_attributes_without_cleanup, :email_issue_attributes
-        alias_method :email_issue_attributes, :email_issue_attributes_with_cleanup
-      end
-    end
-
-    def email_issue_attributes_with_cleanup(issue, user, html)
-      items = email_issue_attributes_without_cleanup(issue, user, html)
-
-      return items unless RedmineTidyEmails.settings['remove_empty_fields']
-      if html
-        items.reject! {|item| item =~ /\<strong\>.*:\ \<\/strong\>\z/ }
-      else
-        items.reject! {|item| item =~ /^.*:\ \z/ }
-      end
-      items
-    end
-
-    private
-
-  end
-
   module MailerPatch
     def self.included(base)
       base.class_eval do
@@ -33,7 +7,7 @@ module RedmineTidyEmails
       end
     end
 
-   def issue_edit_with_flags(*args)
+    def issue_edit_with_flags(*args)
       @skip_header = RedmineTidyEmails.settings['remove_header_on_update'] == 'true'
       @skip_body = RedmineTidyEmails.settings['remove_issue_edit_description'] == 'true'
       issue_edit_without_flags(*args)
